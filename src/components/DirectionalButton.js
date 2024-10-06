e2f137174971a30045d93011aa536f5a1578591b
pkrisko <patrick.krisko@gmail.com>
2024-10-06 14:22:38 -0400

    Cleanup

---
+++ src/components/DirectionalButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const DirectionalButton = ({ onClick, className, children }) => {
+  return (
+    <button className={classNames("bg-gray-800 text-white px-4 py-2 rounded", className)} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default DirectionalButton;
--- src/components/Movement.js
+++ src/components/Movement.js
@@ -1,11 +1,17 @@
 'use client';
 
 import React, { useRef } from 'react';
-import { ArrowDown, ArrowLeft, ArrowRight, ArrowUp } from '@phosphor-icons/react';
+import {
+  ArrowDown,
+  ArrowLeft,
+  ArrowRight,
+  ArrowUp,
+} from '@phosphor-icons/react';
 import { Canvas } from '@react-three/fiber';
 import Gear from '@/components/Gear';
 import { OrbitControls } from '@react-three/drei';
 import CameraController from '@/components/CameraController';
+import DirectionalButton from '@/components/DirectionalButton';
 
 function Movement() {
   const controlsRef = useRef();
@@ -15,80 +21,73 @@ function Movement() {
     const controls = controlsRef.current;
     if (!controls) return;
     const { object: camera, target } = controls;
-      switch (direction) {
-        case 'up':
-          camera.position.y += moveAmount;
-          target.y += moveAmount;
-          break;
-        case 'down':
-          camera.position.y -= moveAmount;
-          target.y -= moveAmount;
-          break;
-        case 'left':
-          camera.position.x -= moveAmount;
-          target.x -= moveAmount;
-          break;
-        case 'right':
-          camera.position.x += moveAmount;
-          target.x += moveAmount;
-          break;
-        default:
-          break;
-      }
-      controls.update();
+    switch (direction) {
+      case 'up':
+        camera.position.y += moveAmount;
+        target.y += moveAmount;
+        break;
+      case 'down':
+        camera.position.y -= moveAmount;
+        target.y -= moveAmount;
+        break;
+      case 'left':
+        camera.position.x -= moveAmount;
+        target.x -= moveAmount;
+        break;
+      case 'right':
+        camera.position.x += moveAmount;
+        target.x += moveAmount;
+        break;
+      default:
+        break;
+    }
+    controls.update();
   };
 
   // Calculate rotation speeds based on teeth count
-  const gear1Teeth = 80;
-  const gear2Teeth = 24;
+  const gearOneTeethCount = 80;
+  const gearTwoTeethCount = 24;
   const baseRotationSpeed = 0.5;
-  const gear2RotationSpeed = -((gear1Teeth / gear2Teeth) * baseRotationSpeed);
+  const gearTwoRotationalVelocity = -((gearOneTeethCount / gearTwoTeethCount) * baseRotationSpeed);
 
   return (
     <div className="w-full h-screen relative">
-      {/* Directional Pad */}
-      <div className="absolute bottom-5 z-50 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
-        <button
-          className="bg-gray-800 text-white px-4 py-2 rounded mb-2"
-          onClick={() => moveCamera('up')}
-        >
+      {/* Directional Pad. Positioned on HTML body, z-index above the canvas. */}
+      <div className="absolute bottom-5 z-10 left-1/2 transform -translate-x-1/2 grid grid-cols-3 gap-4">
+        <DirectionalButton onClick={() => moveCamera('up')} className="col-start-2 row-start-1">
           <ArrowUp size={32} />
-        </button>
-        <div className="flex space-x-2">
-          <button
-            className="bg-gray-800 text-white px-4 py-2 rounded"
-            onClick={() => moveCamera('left')}
-          >
-            <ArrowLeft size={32} />
-          </button>
-          <button
-            className="bg-gray-800 text-white px-4 py-2 rounded"
-            onClick={() => moveCamera('down')}
-          >
-            <ArrowDown size={32} />
-          </button>
-          <button
-            className="bg-gray-800 text-white px-4 py-2 rounded"
-            onClick={() => moveCamera('right')}
-          >
-            <ArrowRight size={32} />
-          </button>
-        </div>
+        </DirectionalButton>
+        <DirectionalButton onClick={() => moveCamera('left')} className="col-start-1 row-start-2">
+          <ArrowLeft size={32} />
+        </DirectionalButton>
+        <DirectionalButton onClick={() => moveCamera('down')} className="col-start-2 row-start-2">
+          <ArrowDown size={32} />
+        </DirectionalButton>
+        <DirectionalButton onClick={() => moveCamera('right')} className="col-start-3 row-start-2">
+          <ArrowRight size={32} />
+        </DirectionalButton>
       </div>
 
       <Canvas shadows camera={{ position: [0, 0, 100], fov: 60 }}>
-        <CameraController moveCamera={moveCamera} controlsRef={controlsRef} />
+        <CameraController
+          moveCamera={moveCamera}
+          controlsRef={controlsRef}
+        />
 
         {/* Lighting */}
         <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 10]} intensity={1} castShadow />
+        <directionalLight
+          position={[10, 10, 10]}
+          intensity={1}
+          castShadow
+        />
 
         {/* Gears */}
         <Gear
           position={[-2.5, 0, 0]}
           radius={20}
           innerRadius={19}
-          teeth={gear1Teeth}
+          teeth={gearOneTeethCount}
           toothDepth={1}
           thickness={2}
           rotationSpeed={baseRotationSpeed}
@@ -98,10 +97,10 @@ function Movement() {
           position={[-29.7, 1, 0]}
           radius={6}
           innerRadius={5}
-          teeth={gear2Teeth}
+          teeth={gearTwoTeethCount}
           toothDepth={1}
           thickness={2}
-          rotationSpeed={gear2RotationSpeed}
+          rotationSpeed={gearTwoRotationalVelocity}
           color="teal"
         />
 
